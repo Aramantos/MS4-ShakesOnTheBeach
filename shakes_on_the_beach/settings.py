fac81155289d7184396de05a268652f4a145a74c
Aramantos <john.doyle.mail@icloud.com>
2021-07-31 13:11:31 +0000

    update ALLOWED_HOSTS in settings.py

--- shakes_on_the_beach/settings.py
+++ shakes_on_the_beach/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['shakes-on-the-beach.herokuapp.com', 'localhost']
 
 # Application definition
 
