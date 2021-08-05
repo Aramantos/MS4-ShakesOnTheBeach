5de06d06fbf1e24d44b72055bfbc1f630c4bb811
Aramantos <john.doyle.mail@icloud.com>
2021-08-05 16:18:29 +0000

    add menu_product_details functionality & template

--- menu/urls.py
+++ menu/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.menu, name='menu')
+    path('', views.menu, name='menu'),
+    path('<product_id>', views.menu_product_detail, name='menu_product_detail'),
 ]
--- menu/views.py
+++ menu/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from .models import Product
 
@@ -13,3 +13,15 @@ def menu(request):
     }
 
     return render(request, 'menu/menu.html', context)
+
+
+def menu_product_detail(request, product_id):
+    """ A view to show individual menu products """
+
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'menu/menu_product_detail.html', context)
