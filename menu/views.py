1b9f58f4839605740571e5c411612e137295af8c
Aramantos <john.doyle.mail@icloud.com>
2021-08-06 12:59:25 +0000

    add search functionality

--- menu/views.py
+++ menu/views.py
@@ -1,15 +1,35 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
 
-from .models import Product
+from .models import Product, Category
 
 
 def menu(request):
     """ A view to return the menu page """
 
     menu = Product.objects.all()
+    query = None
+    categories = None
+
+    if request.GET:
+        if 'category' in request.GET:
+            categories = request.GET['category']
+            menu = menu.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
+
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You didn't enter any search criteria!")
+                return redirect(reverse('menu'))
+
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            menu = menu.filter(queries)
 
     context = {
         'menu': menu,
+        'search_term': query,
     }
 
     return render(request, 'menu/menu.html', context)
