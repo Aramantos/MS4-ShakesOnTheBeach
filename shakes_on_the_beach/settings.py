fe469e02229ef3fa20fb32b7e20707214cf65594
Aramantos <john.doyle.mail@icloud.com>
2021-08-10 18:22:36 +0000

    add basket contexts and delivery logic

--- shakes_on_the_beach/settings.py
+++ shakes_on_the_beach/settings.py
@@ -61,6 +61,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',  # required by allauth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'basket.contexts.basket_contents',
             ],
         },
     },
@@ -137,6 +138,9 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+FREE_DELIVERY_THRESHOLD = 15
+STANDARD_DELIVERY = 2
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
