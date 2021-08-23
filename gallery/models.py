25d4b112fc8a4a646c29cbdcd74eaa18d2e76a86
Aramantos <john.doyle.mail@icloud.com>
2021-08-23 23:29:51 +0000

    update css styles for the gallery and add_image templates

--- gallery/models.py
+++ gallery/models.py
@@ -6,8 +6,8 @@ class GalleryImages(models.Model):
     class Meta:
         verbose_name_plural = 'Gallery Images'
 
-    image = models.ImageField(null=True, blank=True)
-    order_number = models.CharField(max_length=254, null=True, blank=True)
+    image = models.ImageField(null=False, blank=False)
+    order_number = models.CharField(max_length=254, null=False, blank=False)
 
     def __str__(self):
         return self.order_number
