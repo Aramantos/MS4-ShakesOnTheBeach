b0df755e0dca0338a26c510fdecf37496ac9f7c1
Aramantos <john.doyle.mail@icloud.com>
2021-09-02 10:34:22 +0000

    formatted and validated js files

--- checkout/static/checkout/js/checkout.js
+++ checkout/static/checkout/js/checkout.js
@@ -2,18 +2,18 @@ $(document).ready(function () {
     $('#collection').click(function() {
         $('#collection').prop("disabled", true);
         $('#delivery-btn').prop("disabled", false);
-        $('.delivery').css("display", "none")
-        $('.grand-total').css("display", "none")
-        $('#payment-form').css("display", "none")
-        $('#collection-form').removeClass("d-none")
-        $('#collection-form').css("display", "block")
-    })
+        $('.delivery').css("display", "none");
+        $('.grand-total').css("display", "none");
+        $('#payment-form').css("display", "none");
+        $('#collection-form').removeClass("d-none");
+        $('#collection-form').css("display", "block");
+    });
     $('#delivery-btn').click(function() {
         $('#delivery-btn').prop("disabled", true);
         $('#collection').prop("disabled", false);
-        $('.delivery').css("display", "block")
-        $('.grand-total').css("display", "block")
-        $('#collection-form').css("display", "none")
-        $('#payment-form').css("display", "block")
-    })
+        $('.delivery').css("display", "block");
+        $('.grand-total').css("display", "block");
+        $('#collection-form').css("display", "none");
+        $('#payment-form').css("display", "block");
+    });
 });
--- static/js/base.js
+++ static/js/base.js
@@ -3,18 +3,18 @@ $(document).ready(function () {
         $('#eye').css("opacity", "1");
     }, function() {
         $('#eye').css("opacity", "0");
-    })
+    });
     $('#eye').click(function() {
-        $('.container').css("display", "none")
-        $('.container-fluid').css("display", "none")
+        $('.container').css("display", "none");
+        $('.container-fluid').css("display", "none");
         $('#eye').css("display", "none");
         $('#eye-x').css("display", "block");
-    })
+    });
     $('#eye-x').click(function() {
-        $('.container').css("display", "block")
-        $('.container-fluid').css("display", "block")
+        $('.container').css("display", "block");
+        $('.container-fluid').css("display", "block");
         $('#eye').css("display", "block");
         $('#eye').css("opacity", "0");
         $('#eye-x').css("display", "none");
-    })
+    });
 });
