0c5d27cc333f263ddd33fd596607993e265dafc0
Aramantos <john.doyle.mail@icloud.com>
2021-09-02 00:12:38 +0000

    removed commented out code in checkout view

--- checkout/views.py
+++ checkout/views.py
@@ -171,30 +171,6 @@ def checkout_success(request, order_number):
 
     return render(request, template, context)
 
-
-# class collection_Handler:
-#     """Handle collectiong orders"""
-
-#     def __init__(self, request):
-#         self.request = request
-
-#     def _send_collection_confirmation_email(self, order):
-#         """Send the user a confirmation email"""
-#         cust_email = order.email
-#         subject = render_to_string(
-#             'checkout/confirmation_emails/confirmation_email_subject.txt',
-#             {'order': order})
-#         body = render_to_string(
-#             'checkout/confirmation_emails/collection_confirmation_email_body.txt',
-#             {'order': order, 'contact_email': settings.DEFAULT_FROM_EMAIL})
-
-#         send_mail(
-#             subject,
-#             body,
-#             settings.DEFAULT_FROM_EMAIL,
-#             [cust_email]
-#         )
-
 def checkout_collection(request):
     if request.method == 'POST':
         basket = request.session.get('basket', {})
